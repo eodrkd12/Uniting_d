ec91b2c90aef02706a6769fc331ca8b7efab51de
eodrkd12 <eodrkd12@gmail.com>
2019-11-08 19:40:10 +0900

    ㅇㅇ

--- Server/routes/department.js
+++ Server/routes/department.js
@@ -3,7 +3,7 @@ var router=express.Router();
 
 var db_department=require('../public/SQL/department_sql')();
 
-router.get('/',function(req,res,next){
+router.post('/',function(req,res,next){
     db_department.get_department(req.body.university_name,req.body.department_name,function(err,result){
         if(err) console.log(err);
         else res.send(result);
