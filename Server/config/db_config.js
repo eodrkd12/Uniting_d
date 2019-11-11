98aa00820cf2e630255f273d711b075336200065
eodrkd12 <eodrkd12@gmail.com>
2019-11-11 09:37:42 +0000

    aa

--- Server/config/db_config.js
+++ Server/config/db_config.js
@@ -4,9 +4,9 @@ module.exports = function () {
     var pool = mysql.createPool({
         host: '127.0.0.1',
         port: 3306,
-        user: 'root',
-        password: 'dnflwlq',
-        database: 'Uniting',
+        user: 'uniting',
+        password: 'app1!',
+        database: 'uniting',
         connectionLimit: 50,
         queueLimit: 100
     });
