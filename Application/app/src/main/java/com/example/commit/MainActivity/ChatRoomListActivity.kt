3d23a7d7e7968b607f73b934ab8770f21da84bfa
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2019-12-20 16:23:17 +0900

    Update ChatRoomListActivity.kt

--- Application/app/src/main/java/com/example/commit/MainActivity/ChatRoomListActivity.kt
+++ Application/app/src/main/java/com/example/commit/MainActivity/ChatRoomListActivity.kt
@@ -1,5 +1,6 @@
 package com.example.commit.MainActivity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -14,8 +15,9 @@ import com.example.commit.Adapter.ChatRoomListAdapter
 import com.example.commit.ListItem.ChatRoomListItem
 import com.example.commit.R
 import com.example.commit.Singleton.VolleyService
-import kotlinx.android.synthetic.main.activity_chat_room_list.*
-import kotlinx.android.synthetic.main.activity_chat_room_list.list_chat_room
+import kotlinx.android.synthetic.main.activity_open_chat_list.*
+import kotlinx.android.synthetic.main.activity_open_chat_list.list_chat_room
+import kotlinx.android.synthetic.main.activity_open_chat_list.*
 import kotlinx.android.synthetic.main.fragment_chat.*
 import org.json.JSONArray
 import org.json.JSONObject
@@ -24,9 +26,10 @@ class ChatRoomListActivity : AppCompatActivity() {
 
     lateinit var rvCategory: RecyclerView
 
+    @SuppressLint("WrongConstant")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_chat_room_list)
+        setContentView(R.layout.activity_open_chat_list)
 
         val listChatRoom=findViewById<ListView>(R.id.list_chat_room)
 
