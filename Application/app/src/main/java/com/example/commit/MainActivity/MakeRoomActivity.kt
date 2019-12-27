5ba5a81c0fd58179fdfb09bd090ab906862686e5
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2019-12-27 23:34:52 +0900

    오픈채팅 방만들기

--- Application/app/src/main/java/com/example/commit/MainActivity/MakeRoomActivity.kt
+++ Application/app/src/main/java/com/example/commit/MainActivity/MakeRoomActivity.kt
@@ -1,13 +1,33 @@
 package com.example.commit.MainActivity
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.example.commit.R
+import com.example.commit.Singleton.VolleyService
+import kotlinx.android.synthetic.main.activity_makeroom.*
 
 class MakeRoomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_makeroom)
+
+      /*btn_room.setOnClickListener {
+            var name = text_room_name.text.toString()
+            var about= text_room_intro.text.toString()
+
+            Log.d("test",name)
+            Log.d("test",about)
+
+            VolleyService.joinReq(name,about,this,{ success->
+                if(success.equals("success")) {
+                    var intent = Intent(this,OpenChatListActivity::class.java)
+                    startActivity(intent)
+                }
+             }
+        })
+     }*/
     }
 }
