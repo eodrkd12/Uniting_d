a1ecd12b64cdadc32c3fde0bd931f916db9d762b
lilly9928 <50159938+lilly9928@users.noreply.github.com>
2020-01-06 20:44:27 +0900

    방만들기

--- Application/app/src/main/java/com/example/commit/MainActivity/MakeRoomActivity.kt
+++ Application/app/src/main/java/com/example/commit/MainActivity/MakeRoomActivity.kt
@@ -13,54 +13,41 @@ import com.example.commit.Class.UserInfo
 import com.example.commit.R
 import com.example.commit.Singleton.VolleyService
 import kotlinx.android.synthetic.main.activity_makeroom.*
+import kotlinx.android.synthetic.main.activity_my_post.*
 
 class MakeRoomActivity : AppCompatActivity() {
 
-    /* var btn_out="";
-     var rg="";*/
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_makeroom)
 
 
+        rdgroup.check(R.id.r_btn_category1)
 
-     /*   rg = (RadioGroup) findViewById(R.id.rdgroup);
+        btn_room.setOnClickListener {
+            var clicked_rbtn = findViewById<RadioButton>(rdgroup.checkedRadioButtonId)
 
-        btn_out = (Button) findViewById(R.id.btn_out);
-        btn_out.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                //public int getCheckedRadioButtonId () : 선택된 라디오버튼의 ID값을 반환
-                RadioButton rd = (RadioButton) findViewById(rg.getCheckedRadioButtonId());
-                var str_Qtype = rd.getText().toString();
-
-
-                Toast.makeText(getApplicationContext(), str_Qtype+" 선택됨",
-                    Toast.LENGTH_SHORT).show();
-            }
-        });*/
-
-
-      btn_room.setOnClickListener {
             var roomTitle = text_room_name.text.toString()
-            var about= text_room_intro.text.toString()
-            var category = " "                              //카테고리 체크시 text 받아오기 체크 하나씩만
-           var  maxrange = text_max.text.toString()
+            var about = text_room_intro.text.toString()
+            var category = clicked_rbtn.text.toString()                             //카테고리 체크시 text 받아오기 체크 하나씩만
+            var maxrange = text_max.text.toString().toInt()
 
 
-            Log.d("test",roomTitle)
-            Log.d("test",about)
 
 
-          VolleyService.rcreateOpenChatReq(UserInfo.NICKNAME,roomTitle,category,UserInfo.UNIV,maxrange,this,{ success ->
-                if(success.equals("success")) {
-                    var intent = Intent(this,OpenChatListActivity::class.java)
-                    startActivity(intent)
-                }
+            Log.d("test", roomTitle)
+            Log.d("test", about)
+            Log.d("test", category)
 
-        })
-    }
+
+          /*  VolleyService.createOpenChatReq(UserInfo.NICKNAME,roomTitle,category,UserInfo.UNIV,maxrange,this,{ success ->
+                  if(success.equals("success")) {
+                      var intent = Intent(this,OpenChatListActivity::class.java)
+                      startActivity(intent)
+                  }
+
+          })*/
+        }
     }
 }
